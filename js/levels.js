dcf80e78894c2a17161b3d24763786fa5ff0d779
MiraNovotny92 <158027195+MiraNovotny92@users.noreply.github.com>
2026-08-12 16:44:11 +0200

    Update levels.js

--- js/levels.js
+++ js/levels.js
@@ -1,100 +1,36 @@
 function buildMissionLevel(difficultyMultiplier) {
-    // Reset all arrays for the new level
-    GAME.platforms = []; GAME.movingPlatforms = []; GAME.jumpPads = []; GAME.forceContainers = []; 
-    GAME.stars = []; GAME.vaporators = []; GAME.crates = []; GAME.studs = []; GAME.droids = []; 
-    GAME.particles = []; GAME.kyberCrystals = []; GAME.waterPits = []; GAME.buildings = [];
-    GAME.laserGates = []; GAME.npcs = [];
+    // 1. Reset all arrays for the new level
+    GAME.platforms = []; 
+    GAME.movingPlatforms = []; 
+    GAME.jumpPads = []; 
+    GAME.forceContainers = []; 
+    GAME.stars = []; 
+    GAME.vaporators = []; 
+    GAME.crates = []; 
+    GAME.studs = []; 
+    GAME.droids = []; 
+    GAME.particles = []; 
+    GAME.kyberCrystals = []; 
+    GAME.waterPits = []; 
+    GAME.buildings = [];
+    GAME.laserGates = []; 
+    GAME.npcs = [];
     
     GAME.kyberCrystalsCollected = 0;
-    let cursorX = 0;
 
-    // Background Stars
+    // 2. Generate Background Stars (Applies to all levels)
     for(let i = 0; i < 250; i++) {
         GAME.stars.push({ x: Math.random() * 8000, y: Math.random() * 450, size: Math.random() * 2 + 1, alpha: Math.random() });
     }
 
-    // --- HELPER FUNCTIONS TO BUILD LEVELS ---
-    function addGround(width, heightY = 540) {
-        GAME.platforms.push({ x: cursorX, y: heightY, width: width, height: 600 - heightY + 100, isGround: true });
-        cursorX += width;
-    }
-    function addWaterPit(width) {
-        GAME.waterPits.push({ x: cursorX, width: width, y: 550 });
-        cursorX += width;
-    }
-
-    // ==========================================
-    // LEVEL 1: FIND OBI-WAN'S LIGHTSABER
-    // ==========================================
+    // 3. Route to the correct handcrafted mission file
     if (GAME.currentMission === 1) {
-        
-        // 1. Safe starting area with Obi-Wan
-        GAME.obi = { x: 300, y: 490, width: 50, height: 50 };
-        addGround(800, 540); 
-
-        // 2. A simple water pit jump
-        addWaterPit(200);
-        
-        // 3. A platform with an R2D2 Droid
-        GAME.droids.push({
-            x: cursorX + 100, y: 495, baseY: 495, width: 40, height: 45, type: 'r2d2',
-            dx: 1.5, minX: cursorX + 50, maxX: cursorX + 350,
-            bounceY: 0, textTimer: 0, label: 'Beep Boop!', isFloating: false
-        });
-        addGround(400, 540);
-
-        // 4. Force Container to lift out of the way
-        GAME.forceContainers.push({
-            x: cursorX + 150, y: 310, width: 90, height: 230,
-            baseY: 310, isHovering: false, type: 'brick', color: '#e74c3c'
-        });
-        addGround(600, 540);
-// A stretch of ground with BB-8 and a Tree
-GAME.vaporators.push({ x: cursorX + 200, y: 360, width: 30, height: 180 });
-GAME.droids.push({
-    x: cursorX + 300, y: 495, baseY: 495, width: 40, height: 45, 
-    type: 'bb8', dx: 1.5, minX: cursorX + 250, maxX: cursorX + 450,
-    bounceY: 0, textTimer: 0, label: 'Beep!', isFloating: false
-});
-addGround(600, 540);
-
-// A water pit
-addWaterPit(250);
-
-// More ground with a Force Block
-GAME.forceContainers.push({
-    x: cursorX + 100, y: 310, width: 100, height: 160,
-    baseY: 310, isHovering: false, type: 'speeder'
-});
-addGround(500, 540);
-
-        // A stretch of ground with BB-8 and a Tree
-GAME.vaporators.push({ x: cursorX + 200, y: 360, width: 30, height: 180 });
-GAME.droids.push({
-    x: cursorX + 300, y: 495, baseY: 495, width: 40, height: 45, 
-    type: 'bb8', dx: 1.5, minX: cursorX + 250, maxX: cursorX + 450,
-    bounceY: 0, textTimer: 0, label: 'Beep!', isFloating: false
-});
-addGround(600, 540);
-
-// A water pit
-addWaterPit(250);
-
-// More ground with a Force Block
-GAME.forceContainers.push({
-    x: cursorX + 100, y: 310, width: 100, height: 160,
-    baseY: 310, isHovering: false, type: 'speeder'
-});
-addGround(500, 540);
-        // 5. Goal Area with the Lightsaber!
-        GAME.lightsaber = { x: cursorX + 200, y: 480, width: 12, height: 60 };
-        addGround(500, 540);
-
-        GAME.worldWidth = cursorX;
+        buildMission1(difficultyMultiplier);
     } 
-    // Basic flat ground for Mission 2 & 3 (until we build them!)
-    else {
-        addGround(2000, 540);
-        GAME.worldWidth = cursorX;
+    else if (GAME.currentMission === 2) {
+        // buildMission2(difficultyMultiplier); // We will create this later
+    } 
+    else if (GAME.currentMission === 3) {
+        // buildMission3(difficultyMultiplier); // We will create this later
     }
 }
