e582524d7cf6a18c4bdb548fa30ae50bde5ff5a3
MiraNovotny92 <158027195+MiraNovotny92@users.noreply.github.com>
2026-08-12 20:44:16 +0200

    Update levels.js

--- js/levels.js
+++ js/levels.js
@@ -1,36 +1,25 @@
-function buildMissionLevel(difficultyMultiplier) {
+function buildMissionLevel() {
     // 1. Reset all arrays for the new level
-    GAME.platforms = []; 
-    GAME.movingPlatforms = []; 
-    GAME.jumpPads = []; 
-    GAME.forceContainers = []; 
-    GAME.stars = []; 
-    GAME.vaporators = []; 
-    GAME.crates = []; 
-    GAME.studs = []; 
-    GAME.droids = []; 
-    GAME.particles = []; 
-    GAME.kyberCrystals = []; 
-    GAME.waterPits = []; 
-    GAME.buildings = [];
-    GAME.laserGates = []; 
-    GAME.npcs = [];
+    GAME.platforms = []; GAME.movingPlatforms = []; GAME.jumpPads = []; GAME.forceContainers = []; 
+    GAME.stars = []; GAME.vaporators = []; GAME.crates = []; GAME.studs = []; GAME.droids = []; 
+    GAME.particles = []; GAME.kyberCrystals = []; GAME.waterPits = []; GAME.buildings = [];
+    GAME.laserGates = []; GAME.npcs = [];
     
     GAME.kyberCrystalsCollected = 0;
 
-    // 2. Generate Background Stars (Applies to all levels)
+    // 2. Generate Background Stars
     for(let i = 0; i < 250; i++) {
         GAME.stars.push({ x: Math.random() * 8000, y: Math.random() * 450, size: Math.random() * 2 + 1, alpha: Math.random() });
     }
 
     // 3. Route to the correct handcrafted mission file
     if (GAME.currentMission === 1) {
-        buildMission1(difficultyMultiplier);
+        buildMission1();
     } 
     else if (GAME.currentMission === 2) {
-        // buildMission2(difficultyMultiplier); // We will create this later
+        buildMission2(); 
     } 
     else if (GAME.currentMission === 3) {
-        // buildMission3(difficultyMultiplier); // We will create this later
+        buildMission3(); 
     }
 }
